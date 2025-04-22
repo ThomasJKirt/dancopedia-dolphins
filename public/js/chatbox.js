5854a86a4fe8ea130426fed1a5ba314b15af20b9
TuggyBoat <39076583+TuggyBoat@users.noreply.github.com>
2025-04-22 15:35:57 -0500

    Secure chatbot

--- public/js/chatbox.js
+++ public/js/chatbox.js
@@ -55,7 +55,7 @@ function initializeChatbox() {
         inputField.value = "";
 
         try {
-            const response = await fetch("http://dancopedia-brazil.com/chat", {
+            const response = await fetch("https://dancopedia-brazil.com/chat", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ message: messageText }),
