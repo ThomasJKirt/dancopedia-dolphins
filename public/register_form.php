2149583b3a9f8c9f60d807ac8ba7861dfa38b336
TuggyBoat <39076583+TuggyBoat@users.noreply.github.com>
2025-04-23 18:21:46 -0500

    Fix register

--- public/register_form.php
+++ public/register_form.php
@@ -38,6 +38,7 @@ if (isset($_POST['submit'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>register form</title>
     <link rel="stylesheet" href="css/Register.css">
+    <link rel="stylesheet" href="css/Login.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
